c705c8c31910014dfaf2775312f6eb5a249fa882
mohammedraees07 <mohammedraees004@gmail.com>
2026-09-04 12:14:58 +0530

    fix url update to fetch blogs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mahatienterprises.com',
+  site: 'https://dashboard.mahatienterprises.com',
   trailingSlash: 'never',
   build: {
     format: 'file'
